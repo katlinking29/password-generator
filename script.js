2190595beb4b9df10af8dcab2896fd62f572f5fc
Katlin King <katlinking29@gmail.com>
2020-05-09 17:03:36 -0400

    reorganized conditionals into my for loop and updated the random number function so it would be repeated in the for loop

--- script.js
+++ script.js
@@ -16,82 +16,66 @@ generateBtn.addEventListener("click", writePassword);
 // Variable to store password character output options
 var alphaCapital = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] 
 var alphaLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var number = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var num = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var spCharacter = ["~", "!", "#", "$", "%", "^", "&", "*", "-", "+", ",", "'", "*", "=", "/", ":", "<", ">", "?", "\",;", "[", "]", "{", "}"]
 
-// User input to set password parameters
-var userPasswordCapital= confirm("Would you like to include capital letters in your password?")
+// defining the generatePassword function so that when I click the button the password is input and able to be viewed on the DOM
+function generatePassword() {
+
+// setting a variable to contain the password as a string
+var thePassword = ("")
+
+// creating a function that generates a random number for the indexes I will pull from my arrays based on user input
+function randomNumber(pw) {
+  return (Math.floor(Math.random() * pw))
+}
+// Does the user want to inlcude capital letters in their password?
+var userPasswordCase= confirm("Would you like to include capital letters in your password?")
+
+// Does the user want to inlcude numbers in their password?
 var userPasswordNumber = confirm("Would you like to include numbers in your password?")
-var userPasswordSpCharacter= confirm("Would you like to include special characters in your password?")
-var userPasswordLength = prompt("How many characters would you like to include in your password?")
+console.log(userPasswordNumber)
 
-
-// Based on the user's input, return random indices from the arrays
-var passwordParameters = {
-  // Does the user want to inlcude capital letters in their password?
-  letterCase: function() {
-
-      if (userPasswordCapital === true) {
-         return Math.floor(Math.random() * alphaCapital.length)
-        // pull from the capital array; use mathrandom to choose a random number, and then when you call the function, you'll do the array name.index number from the array (random number)
-      }
-  
-      else if (userPasswordCapital === false) {
-         return Math.floor(Math.random() * alphaLowerCase.length)
-        // pull from the alphalowercase array
-      }
-  },
-  // Does the user want to inlcude numbers in their password?
-  numberCheck: function() {
-      if (userPasswordNumber === true) {
-         return Math.floor(Math.random() * number.length)
-     }
-
-     else if (userPasswordNumber === false) {
-      return ("null")
-   }
-  },
 // Does the user want to include special characters in their password?
-  spCharacterCheck: function() {
+var userPasswordSpCharacter= confirm("Would you like to include special characters in your password?")
 
-      if (userPasswordSpCharacter === true) {
-          return Math.floor(Math.random() * spCharacter.length)
-       } 
-
-       else if (userPasswordSpCharacter === false) {
-        return ("null")
-     }
-  },
 // How many characters does the user want their password to be?
-  passwordLength: function() {
-
+var userPasswordLength = prompt("How many characters would you like to include in your password?")
     if (userPasswordLength < 8) {
       alert("Your password must be at least 8 characters long"); 
       prompt("How many characters would you like to include in your password?");
-      return(this.userPasswordLength)
-      // how do I adjust the value for userPasswordLength based on the user's prompt input?
     }
 
     else if (userPasswordLength > 128) {
       alert("Your password cannot exceed 128 characters"); 
       prompt("How many characters would you like to include in your password?");
-      return this.userPasswordLength
     } 
 
-    else if (userPasswordLength > 8 || userPasswordLength < 128 || userPasswordLength == 8 || userPasswordLength == 128) {
-      return userPasswordLength
+// A for looop is added so that I loop through code above and pull the indices from my arrays. It will loop through based on the password length the user chooses
+  for (var i=0; i < userPasswordLength; i++) {
+
+    // choose a value from the alphaCapital array based on if the user input is true 
+    if (userPasswordCase === true) {
+      var passwordOutput = alphaCapital[randomNumber(alphaCapital.length)]
     }
+
+    // choose a value from the num array based on if the user input is true 
+    else if (userPasswordNumber === true) {
+      var passwordOutput = num[randomNumber(num.length)]
+    }
+
+    // choose a value from the spCharacter array based on if the user input is true 
+    else if (userPasswordSpCharacter === true) {
+      var passwordOutput = spCharacter[randomNumber(spCharacter.length)]
+    }
+
+    // choose a value from the alphalowerCase arrays based on if the user input is false for uppercase letters
+    else if (userPasswordCase === false) {
+      var passwordOutput = alphaLowerCase[randomNumber(alphaLowerCase.length)]
+    }
+
+    thePassword = thePassword + passwordOutput 
   }
-}
-
-
-for (var i=0; i < userPasswordLength; i++) {
-
-console.log(passwordParameters.letterCase())
-console.log(passwordParameters.numberCheck())
-console.log(passwordParameters.spCharacterCheck())
-console.log(passwordParameters.passwordLength())
-
-}
-
-// Next steps, I need to input the index value from my arrays, based on the user's selections. 
+  
+  return thePassword
+  }
